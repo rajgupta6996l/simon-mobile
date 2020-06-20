156d9413963087842cad003c3b12a02af5c5de6e
rajgupta6996l <62673025+rajgupta6996l@users.noreply.github.com>
2020-06-20 15:06:21 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,79 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+//Sound function
+function playSound(name) {
+  var sound = new Audio("sounds/" + name + ".mp3");
+  sound.play();
+}
+//Random button animation
+function buttonFlash(name) {
+  $("#" + name).fadeOut(100).fadeIn(100);
+}
+//User-clicked button animation
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed")
+  }, 100);
+}
+//Reset Value Function
+function resetValues() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+}
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  buttonFlash(randomChosenColor);
+  playSound(randomChosenColor);
+  level++;
+  $("#level-title").text("Level " + level);
+  userClickedPattern = [];
+};
+
+
+//The Game
+$(document).one("keydown", playgame);
+$(document).one("swipe", playgame);
+
+function playgame() {
+  nextSequence();
+  $(".btn").on("click",userInput);
+  $(".btn").on("tap",userInput);
+  function userInput() {
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  }
+
+  function checkAnswer(currentClicked) {
+    if (userClickedPattern[currentClicked] === gamePattern[currentClicked]) {
+      if (userClickedPattern.length === gamePattern.length) {
+        setTimeout(function() {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("#level-title").text("Game Over, Press any key or Swipe left/right to Restart!");
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $(".btn").off("click");
+      $(".btn").off("tap");
+      $(document).one("keydown", playgame);
+      $(document).one("swipe", playgame);
+      resetValues();
+    }
+  }
+}
